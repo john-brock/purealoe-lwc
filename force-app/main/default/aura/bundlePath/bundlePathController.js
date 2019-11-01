b20ab2296686fcd23542bb8969b47457774f110a
John Brock <_johnbrock@outlook.com>
2019-11-01 13:56:19 -0700

    BRK DF19 Initial Changes

--- force-app/main/default/aura/bundlePath/bundlePathController.js
+++ force-app/main/default/aura/bundlePath/bundlePathController.js
@@ -2,16 +2,20 @@
     subscribe: function(component, event) {
         var empApi = component.find('empApi');
         var channel = component.get('v.channel');
-        var replayId = -2;
+        var replayId = -1;
         empApi.subscribe(
             channel,
             replayId,
             $A.getCallback(function(message) {
+                console.log(message);
+                console.log(message.data.payload.ChangeEventHeader.recordIds[0]);
+                console.log(message.data.payload.Status__c);
                 var bundle = component.get('v.record');
-                if (message && message.data && message.data.sobject) {
-                    var bundleId = message.data.sobject.Id;
-                    var status = message.data.sobject.Status__c;
+                if (message && message.data && message.data.payload && message.data.payload.ChangeEventHeader.recordIds[0]) {
+                    var bundleId = message.data.payload.ChangeEventHeader.recordIds[0];
+                    var status = message.data.payload.Status__c;
                     if (bundleId === bundle.Id && status !== bundle.Status__c) {
+                        console.log('Reload requested');
                         component.find('bundleRecord').reloadRecord(true);
                     }
                 }
